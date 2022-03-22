75fd0a68ad221d74e241a08386a5c44fac9e1c9a
vinodh1988 <k.c.vinodh@hotmail.com>
2022-03-22 10:10:32 +0530

    first-commit

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+//javascript does not support variable declaration with datatypes
+//but it support various kinds of data
+//string, number, boolean are the basic data supported by javascript
+
+
+a="Rahul"
+b='rahul'
+c=90
+d=234.43
+e=true
+
+console.log(a,b,c,d,e)
+
+console.log(typeof(a),typeof(b),typeof(c),typeof(d),typeof(e))
+
+//in javascript, array is collection of objects [it can be any kind of object]
+
+f=[433,343,34.34,true,'rahul']
+
+console.log(f,typeof(f))
+
+g={sno:1,name:"Ravi",city:'Chennai'}
+
+console.log(g,typeof(g))
